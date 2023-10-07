c8531c5c64ad4718fbc6f1eab843b0980aa6eee0
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2023-10-07 11:59:57 -0600

    Async-Await comenzamos por try-catch

---
+++ 41-AsyncAwait/js/01-app.js
@@ -0,0 +1,12 @@
+//! Se usa regularmente apara consumir servicios de terceros como seria una consulta a una base de datos o una api incluso a una autenticacion de usuario 
+
+console.log(1+1);
+
+try {
+    autenticarUsuario();
+    
+} catch (error) {
+    console.log(error);
+}
+
+console.log(2+2);
