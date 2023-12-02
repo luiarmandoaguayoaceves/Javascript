4e9de8df874eb11da1d51a61c54584a1812300ff
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2023-12-02 10:44:39 -0600

    jest  aprendiendo a hacer pruebas

---
+++ 51-Testing-Jest/__tests__/holamundo.js
@@ -0,0 +1,7 @@
+test('Hola Mundo en jest', () => {  })
+it('Hola Mundo en jest con it', () => {  })
+
+describe('Grupo de pruebas', () => {
+    test('Hola Mundo en jest', () => {  })
+it('Otro hola Mundo en jest con it', () => {  })
+})
