9527b8430d5b705f03f87e7d6cb71bcfbdf4beab
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2024-01-05 18:07:52 -0600

    estilos con boostrap

--- agenciasViajesNode/index.js
+++ agenciasViajesNode/index.js
@@ -10,6 +10,9 @@ const port = process.env.PORT || 4000;
 // Habilitar PUG
 app.set('view engine', 'pug');
 
+//Definir la carpeta publica
+app.use(express.static('public'));
+
 //Agregar router 
 
 app.use('/', router)
