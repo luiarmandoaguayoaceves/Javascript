28b20343f6177c30fbc6e55cdb75d8e8a2888da3
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2024-01-13 18:28:53 -0600

    validacion del formulario

--- agenciasViajesNode/controllers/testimonialController.js
+++ agenciasViajesNode/controllers/testimonialController.js
@@ -1,6 +1,38 @@
 
 const guardarTestimonial = (req, res) => {
-    
+
+    //Validar
+    const {nombre, correo, mensaje} = req.body;
+
+    const errores = []
+
+    if(nombre.trim() === ''){
+     errores.push({mensaje: 'El Nombre esta vacio'})
+    }
+
+    if(correo.trim() === ''){
+        errores.push({mensaje: 'El Correo esta vacio'})
+       }
+
+    if(mensaje.trim() === ''){
+        errores.push({mensaje: 'El Mensaje esta vacio'})
+    }
+
+    if(errores.length > 0){
+
+        //Mostrar la vista con errores
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            errores,
+            nombre,
+            correo,
+            mensaje
+        })
+    } else {
+        //Almacenarlo en la base de datos
+    }
+
+    console.log(errores);
 }
 
 export {
--- agenciasViajesNode/index.js
+++ agenciasViajesNode/index.js
@@ -24,6 +24,9 @@ app.use( (req, res, next) =>{
     next();
 })
 
+//Agregar body parser para leer los datos del formulario 
+app.use(express.urlencoded({extended: true}));
+
 //Definir la carpeta publica
 app.use(express.static('public'));
 
