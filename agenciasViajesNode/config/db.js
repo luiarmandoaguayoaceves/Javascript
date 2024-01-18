10133247e373c37864bb71225c41483e760f0016
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2024-01-17 18:18:53 -0600

    proyecto con base de datos en filess.io

--- agenciasViajesNode/config/db.js
+++ agenciasViajesNode/config/db.js
@@ -2,12 +2,18 @@ import Sequelize from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config()
 
-const {DB_NAME, DB_USER, DB_PASS, DB_HOST} = process.env
+const {DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_URL} = process.env
 
-const db = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-    host: DB_HOST,
-    port: 5432,
-    dialect: 'postgres',
+const db = new Sequelize(
+    //OnLine
+    DB_URL, {
+
+    //Local
+    // DB_NAME, DB_USER, DB_PASS, {
+    // host: DB_HOST,
+    // port: 5432,
+    // dialect: 'postgres',
+    
     define: {
         timestamps: false
     },
